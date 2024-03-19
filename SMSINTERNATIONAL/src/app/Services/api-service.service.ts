94626e07e405abff46a6d473cd2b9de37a892383
ManognyaReddy20 <bojjamanognya@gmail.com>
2024-03-19 16:01:19 +0530

    Update api-service.service.ts
    Login service method created

--- SMSINTERNATIONAL/src/app/Services/api-service.service.ts
+++ SMSINTERNATIONAL/src/app/Services/api-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILogin, ILoginResponce } from '../Interfaces/Ilogin';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  BaseURL: string = 'http://192.168.29.40:3000/'
+
+  constructor(private HTTP: HttpClient) { }
+
+  login(LoginDetails: ILogin): Observable<ILoginResponce> {
+    return this.HTTP.post<ILoginResponce>(`${this.BaseURL}login`, LoginDetails)
+  }
+
 }
