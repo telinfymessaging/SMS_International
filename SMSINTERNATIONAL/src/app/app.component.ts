0d7459ea7c158b30ccd22443dc9692ed40c61fef
apurva3105 <apurvaappu31@gmail.com>
2024-03-20 14:38:56 +0530

    added md5 for login ts
    added md5 for login ts
    
    Co-Authored-By: AbhishekKuchana <125429467+AbhishekKuchana@users.noreply.github.com>

--- SMSINTERNATIONAL/src/app/app.component.ts
+++ SMSINTERNATIONAL/src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import { FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+=======
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+>>>>>>> Stashed changes
 import { RouterOutlet } from '@angular/router';
 
+//intall 'npm install crypto-js' to  use cryptojs
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
+=======
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule, CommonModule],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+<<<<<<< Updated upstream
   title = 'smsInternational';
   islogedin:boolean=false;
   constructor(private fb: FormBuilder) { }
@@ -24,4 +35,27 @@ export class AppComponent {
   Submit() {
     console.log(this.form.value);
   }
+=======
+  islogedin: boolean = false;
+  form = this.fb.group({
+    uname: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._-]{3,20}$')]],
+    pwd: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$')]]
+  });
+  constructor(private fb: FormBuilder) { }
+  login() {
+    // Encrypt (hash) the password
+
+    // Hash the password
+    const hashedPwd = CryptoJS.MD5(String(this.form.value.pwd)).toString();
+    // You might want to create a new object if you need the username unaltered
+    const formData = {
+      uname: this.form.value.uname,
+      pwd: hashedPwd
+    };
+    console.log(formData);
+    // Use formData for further processing, like sending it to your server
+
+  }
+
+>>>>>>> Stashed changes
 }
