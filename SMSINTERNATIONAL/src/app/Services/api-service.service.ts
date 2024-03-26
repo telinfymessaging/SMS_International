6b1965fa325e8594803047857d7103545d1a524b
vinithaBattipati <vinithareddybattepati@gmail.com>
2024-03-26 16:01:06 +0530

    Update api-service.service.ts

--- SMSINTERNATIONAL/src/app/Services/api-service.service.ts
+++ SMSINTERNATIONAL/src/app/Services/api-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ILogin, ILoginResponce } from '../Interfaces/Ilogin';
 import { HttpClient } from '@angular/common/http';
+import { IComposeResponse } from '../Interfaces/IComposeResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,13 @@ export class ApiServiceService {
 
   BaseURL: string = 'http://192.168.29.40:3000/'
 
-  constructor(private HTTP: HttpClient) { }
+  constructor(private objhttp: HttpClient) { }
 
   login(LoginDetails: ILogin): Observable<ILoginResponce> {
-    return this.HTTP.post<ILoginResponce>(`${this.BaseURL}login`, LoginDetails)
+    return this.objhttp.post<ILoginResponce>(`${this.BaseURL}login`, LoginDetails)
   }
-
+  get_compose_details():Observable<IComposeResponse>{
+    return this.objhttp.get<IComposeResponse>(`${this.BaseURL}getcompose`);
+  
+  }
 }
